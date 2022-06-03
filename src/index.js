2f878466bdc7caa2a52ae4d2ab658dec9a33f6c5
yesim <sityesim@gmail.com>
2022-06-03 12:04:00 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm =document.querySelector("#create-task-form")
+  //console.log(taskForm)
+  const taskDescription =document.querySelector("#new-task-description")
+ console.log(taskDescription)
+
+  const taskUl =document.querySelector("#tasks")
+  //console.log(taskUl)
+
+  taskForm.addEventListener("submit", x )
+
 });
+function x(event){
+  event.preventDefault();
+  const taskDescription =document.querySelector("#new-task-description").value
+  const toDoList=document.createElement("li")
+  toDoList.innerHTML=taskDescription;
+  document.getElementById("tasks").appendChild(toDoList);
+  const deleteButton=document.createElement("button")
+deleteButton.innerHTML="DONE"
+deleteButton.style.height="20px";
+deleteButton.style.background="red";
+deleteButton.style.borderRadius="5px";
+
+
+toDoList.appendChild(deleteButton)
+deleteButton.addEventListener("click", reset)
+
+}
+function reset (event){
+  event.preventDefault();
+  this.parentElement.remove()
+  this.parentElement.remove()
+
+}
